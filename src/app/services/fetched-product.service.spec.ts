5dfe438376d82dead50b7b758cbd4c7450b64db1
DavideBernardini <davidebernardini09@gmail.com>
2022-07-22 18:04:45 +0200

    added test service

--- src/app/services/fetched-product.service.spec.ts
+++ src/app/services/fetched-product.service.spec.ts
@@ -1,16 +1,57 @@
-import { TestBed } from '@angular/core/testing';
+import { Product } from 'src/app/interfaces/Product';
+import { TestBed, inject } from '@angular/core/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController
+} from '@angular/common/http/testing';
 
 import { ProductService } from './fetched-product.service';
 
+
+
 describe('ProductService', () => {
-  let service: ProductService;
-
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(ProductService);
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [ProductService],
+    });
   });
+  it('should be created', inject(
+    [ProductService],
+    (service: ProductService) => {
+      expect(service).toBeTruthy();
+    }
+  ));
+  it('should get products', inject(
+    [HttpTestingController, ProductService],
+    (httpMock: HttpTestingController, ProductService: ProductService) => {
+      const mockService = [
+        {
+          id: 12,
+          title: 'pomodoro',
+          price: 200,
+          description: 'description',
+          image: ' https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg',
+        },
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+        {
+          id: 10,
+          title: 'mmmhLoris',
+          price: 300,
+          description: 'description2',
+          image: ' https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg',
+        },
+      ];
+
+      ProductService.fetchProducts().subscribe((data: Product[]) => {
+        expect(data).toEqual(mockService);
+      });
+
+      const req = httpMock.expectOne('https://fakestoreapi.com/products');
+      expect(req.request.method).toBe('GET');
+      req.flush(mockService);
+
+      httpMock.verify();
+    }
+  ));
 });
