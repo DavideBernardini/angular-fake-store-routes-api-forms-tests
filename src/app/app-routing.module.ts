706ae31b5568af1ba30f67e8ab36d89383c051bb
DavideBernardini <davidebernardini09@gmail.com>
2022-07-27 18:51:35 +0200

    added some style to header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,6 @@ import { RouterModule, Routes } from '@angular/router';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
 
 const routes: Routes = [
-  { path: '', component: ProductDetailsComponent },
   { path: 'product/:id', component: ProductDetailsComponent }
 ];
 
