e0f023c35d456fc1f17fda58c37c97a9c9b58448
DavideBernardini <davidebernardini09@gmail.com>
2022-08-03 19:05:00 +0200

    added form and validations

--- src/app/components/product-form/product-form.component.ts
+++ src/app/components/product-form/product-form.component.ts
@@ -1,7 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-
-import { Product } from 'src/app/services/interfaces/Product';
 
 @Component({
   selector: 'app-product-form',
@@ -11,43 +9,53 @@ import { Product } from 'src/app/services/interfaces/Product';
   templateUrl: './product-form.component.html',
   styleUrls: ['./product-form.component.scss']
 })
-export class ProductFormComponent implements OnInit {
+export class ProductFormComponent {
 
   newProductForm: FormGroup;
 
+  categories: string[];
+
   constructor() {
     this.newProductForm = new FormGroup({
-      id: new FormControl(0, [
+      title: new FormControl(null, [
         Validators.required,
-        Validators.min(0)
-      ]),
-      title: new FormControl('', [
-        Validators.required,
-        Validators.minLength(3),
+        Validators.minLength(5),
         Validators.maxLength(150),
       ]),
-      price: new FormControl(0, [
+      price: new FormControl(null, [
         Validators.required,
         Validators.min(0),
-        Validators.max(99999),
       ]),
-      description: new FormControl('', [
+      description: new FormControl(null, [
         Validators.required,
         Validators.minLength(30),
         Validators.maxLength(500),
       ]),
-      category: new FormControl('', [
-        Validators.minLength(3),
-        Validators.maxLength(30),
+      category: new FormControl(null, [
+        Validators.required
       ]),
     });
+
+    this.categories = [
+      "men's clothing",
+      "jewelery",
+      "electronics",
+      "women's clothing"
+    ];
   }
 
   onSubmit() {
-    console.log(this.newProductForm.value);
-  }
+    if (this.newProductForm.invalid) {
 
-  ngOnInit(): void {
+      console.error("Invalid form.");
+
+      Object.keys(this.newProductForm.controls).forEach(key => {
+        console.error(key, this.newProductForm.get(key)?.errors);
+      });
+    }
+
+    console.log(this.newProductForm.valid);
+    console.log(this.newProductForm.value);
   }
 
 }
